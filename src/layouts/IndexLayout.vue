0821224c5a298d0e38b705b177116639fe647bff
HanJiang-cn <2994633504@qq.com>
2025-05-15 19:41:38 +0800

    更新 IndexLayout.vue

--- src/layouts/IndexLayout.vue
+++ src/layouts/IndexLayout.vue
@@ -16,199 +16,184 @@ const menuData = ref([
     id: '1'
   },
   {
-    title: '指南',
+    title: '组织架构',
     url: '',
     id: '2',
     children: [
       {
-        title: '表单组件',
+        title: '创新创业部',
         url: '',
         id: '3-1',
         children: [
           {
-            title: 'Datepicker 日期',
-            url: 'datepicker',
+            title: '创新创业部简介',
+            url: 'introduction',
             id: '3-1-1'
           },
           {
-            title: 'Cascader 级联选择器',
-            url: 'cascader',
+            title: '创新创业部成员',
+            url: 'introduction/members',
             id: '3-1-2'
           },
-          {
-            title: 'DropTimes 下拉时间',
-            url: 'droptimes',
-            id: '3-1-3'
-          }
         ]
       },
       {
-        title: '数据展示',
+        title: '软件部',
         url: '',
         id: '3-2',
         children: [
           {
-            title: 'Card 卡片',
-            url: 'card',
+            title: '软件部简介',
+            url: 'software',
             id: '3-2-1'
           },
           {
-            title: 'Collapse 折叠面板',
-            url: 'collapse',
+            title: '软件部成员',
+            url: 'software/members',
             id: '3-2-2'
-          },
-          {
-            title: 'Guide 引导',
-            url: 'guide',
-            id: '3-2-3'
           }
         ]
       },
       {
-        title: '导航组件',
+        title: '硬件部',
         url: '',
         id: '3-3',
         children: [
           {
-            title: 'ToggleMenu 收缩菜单',
-            url: 'toggleMenu',
+            title: '硬件部简介',
+            url: 'hardware',
             id: '3-3-1'
           },
           {
-            title: 'TreeMenu 树型菜单',
-            url: 'treemenu',
+            title: '硬件部成员',
+            url: 'hardware/members',
             id: '3-3-2'
-          },
-          {
-            title: 'Breadcrumb 面包屑',
-            url: 'breadcrumb',
-            id: '3-3-3'
           }
         ]
       },
       {
-        title: '业务组件',
+        title: '文案部',
         url: '',
         id: '3-4',
         children: [
           {
-            title: 'Amount 金额',
-            url: 'amount',
+            title: '文案部简介',
+            url: 'copywriting',
             id: '3-4-1'
           },
           {
-            title: 'Area 片区',
-            url: 'area',
+            title: '文案部成员',
+            url: 'copywriting/members',
             id: '3-4-2'
+          }
+        ]
+      },
+      {
+        title: '设计部',
+        url: '',
+        id: '3-5',
+        children: [
+          {
+            title: '设计部简介',
+            url: 'design',
+            id: '3-5-1'
           },
           {
-            title: 'Company 公司',
-            url: 'company',
-            id: '3-4-3'
+            title: '设计部成员',
+            url: 'design/members',
+            id: '3-5-2'
           }
         ]
       }
     ]
   },
   {
-    title: '组件',
+    title: '相关成员',
     url: '',
     id: '3',
+  },
+  {
+    title: '项目',
+    url: '',
+    id: '4',
     children: [
       {
-        title: '表单组件',
+        title: '重点项目',
         url: '',
-        id: '3-1',
+        id: '4-1',
         children: [
           {
-            title: 'Datepicker 日期',
-            url: 'datepicker',
-            id: '3-1-1'
+            title: '临界拓池',
+            url: 'ljtc',
+            id: '4-1-1'
           },
           {
-            title: 'Cascader 级联选择器',
-            url: 'cascader',
-            id: '3-1-2'
-          },
-          {
-            title: 'DropTimes 下拉时间',
-            url: 'droptimes',
-            id: '3-1-3'
+            title: '智润田园',
+            url: 'zrty',
+            id: '4-1-2'
           }
         ]
       },
       {
-        title: '数据展示',
+        title: '科研类项目',
         url: '',
-        id: '3-2',
+        id: '4-2',
         children: [
           {
-            title: 'Card 卡片',
-            url: 'card',
-            id: '3-2-1'
+            title: '临界拓池',
+            url: 'ljtc',
+            id: '4-2-1'
           },
           {
-            title: 'Collapse 折叠面板',
-            url: 'collapse',
-            id: '3-2-2'
-          },
-          {
-            title: 'Guide 引导',
-            url: 'guide',
-            id: '3-2-3'
+            title: '风花雪月',
+            url: 'fhxy',
+            id: '4-2-2'
           }
         ]
       },
       {
-        title: '导航组件',
+        title: '竞赛类项目',
         url: '',
-        id: '3-3',
+        id: '4-3',
         children: [
           {
-            title: 'ToggleMenu 收缩菜单',
-            url: 'toggleMenu',
-            id: '3-3-1'
+            title: '临界拓池',
+            url: 'ljtc',
+            id: '4-3-1'
           },
           {
-            title: 'TreeMenu 树型菜单',
-            url: 'treemenu',
-            id: '3-3-2'
+            title: '风花雪月',
+            url: 'fhxy',
+            id: '4-3-2'
           },
           {
-            title: 'Breadcrumb 面包屑',
-            url: 'breadcrumb',
-            id: '3-3-3'
-          }
-        ]
-      },
-      {
-        title: '业务组件',
-        url: '',
-        id: '3-4',
-        children: [
-          {
-            title: 'Amount 金额',
-            url: 'amount',
-            id: '3-4-1'
+            title: '智润田园',
+            url: 'zrty',
+            id: '4-3-3'
           },
           {
-            title: 'Area 片区',
-            url: 'area',
-            id: '3-4-2'
+            title: '星海韵梦',
+            url: 'xhym',
+            id: '4-3-4'
           },
           {
-            title: 'Company 公司',
-            url: 'company',
-            id: '3-4-3'
-          }
+            title: '智能题站',
+            url: 'zrty',
+            id: '4-3-5'
+          },
+          {
+            title: '推星予瞳',
+            url: 'txyt',
+            id: '4-3-6'
+          },
         ]
       }
     ]
   },
   {
     title: '关于实验室',
-    url: 'crop',
-    id: '4'
+    url: '',
+    id: '5',
   }
 ])
 </script>
