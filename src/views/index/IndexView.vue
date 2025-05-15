18820d3100014bcbfcaeff7eafcbc05d90f8d3ae
xu yangyang <2043590802@qq.com>
2025-05-15 19:26:12 +0800

    许

--- src/views/index/IndexView.vue
+++ src/views/index/IndexView.vue
@@ -6,7 +6,6 @@
 import tp1 from '@/assets/pura-x-1.jpg'
 import tp2 from '@/assets/pura-x-2.jpg'
 </script>
-
 <template>
   <a-carousel autoplay>
     <div>
@@ -19,20 +18,25 @@ import tp2 from '@/assets/pura-x-2.jpg'
 </template>
 
 <style scoped lang="less">
-:deep(.slick-track) {
-  height: auto;
+:deep(.slick-slide) {
+  position: relative;
 
-  :deep(.slick-active) {
-    ::before {
-      content: '';
-      position: absolute;
-      display: block;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      height: 200px;
-      background: linear-gradient(rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.2) 100%);
-    }
+  &::before {
+    content: '';
+    position: absolute;
+    display: block;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 200px;
+    background: linear-gradient(rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.5) 100%);
+    z-index: 1;
+  }
+
+  img {
+    display: block;
+    width: 100%;
+    height: auto;
   }
 }
 </style>
